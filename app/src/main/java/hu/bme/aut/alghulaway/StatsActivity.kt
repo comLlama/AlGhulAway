fb8dd21bbe41abe4ff3b53c935b0634cdc4faa79
comLlama <alphalupus846@gmail.com>
2021-12-12 21:30:28 +0100

    tvSumAlc updates properly, still in a wrong place

---
+++ app/src/main/java/hu/bme/aut/alghulaway/StatsActivity.kt
@@ -0,0 +1,22 @@
+package hu.bme.aut.alghulaway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.bme.aut.alghulaway.databinding.ActivityStatsBinding
+import hu.bme.aut.alghulaway.db.DrinkDatabase
+import kotlin.concurrent.thread
+
+class StatsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStatsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStatsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        thread {
+            val alcoholAmount = DrinkDatabase.getDatabase(applicationContext).drinkDao().getAlcoholAmount().toString()
+            runOnUiThread{ binding.textView.text =  alcoholAmount + " " + getString(R.string.unitAlcoholAmount) }
+        }.start()
+    }
+}
