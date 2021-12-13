d3f2a10d2b5906cee98499d3bc953da6d0e00323
comLlama <alphalupus846@gmail.com>
2021-12-13 05:13:04 +0100

    Mock Activities added to MainActivity menu, and fixed stuff

---
+++ app/src/main/java/hu/bme/aut/alghulaway/ArchiveActivity.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.alghulaway.listadapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArchiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_archive)
+    }
+}
